1224c4a128e64271b758ec5198688bf243182037
maxmcgrath1 <tuc06096@temple.edu>
2022-01-04 17:41:04 -0500

    Create Inital Deal function and attached to button

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,23 @@
+// Here I am assigning variables to elements from the HTML
+
 let startGame = document.querySelector('.startGame');
 let welcomeMessage = document.querySelector('.welcomeMessage');
 let clickMessage = document.querySelector('.clickMessage');
 let hitMe = document.querySelector('.hitMe');
+
+// Here I am making a button that clears the welcome messages and starts the game
 
 startGame.addEventListener('click', function () {
     welcomeMessage.innerHTML = '';
     clickMessage.innerHTML = '';
 })
 
-let values = ['2', '3', '4', '5', '6', '7', '8','9', '10', 'J', 'Q', 'K', 'A'];
+// Here I am making arrays of both card values and the suits each value can be attached to
+
+let values = [{'2': 2}, '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
 let suits = ['clubs', 'diamonds', 'spades', 'hearts']
+
+// Here I am creating a deck of 52 objects made of key value pairs from values and suits
 
 let deck = [];
 
@@ -27,8 +35,26 @@ function createDeck() {
 createDeck();
 // console.log(deck);
 
+// Here I am creating a function that will deal a random card from the deck
+
 function randomDeal(deck) {
     let deal = Math.floor(deck.length * Math.random());
     return deck[deal];
 };
-console.log(randomDeal(deck));
+
+randomDeal(deck);
+// console.log(randomDeal(deck));
+
+// Here I am trying to use my randomDeal function to initally deal 2 cards to the dealer and 2 cards to the player.
+
+// console.log(playerHand);
+// console.log(dealerHand);
+
+function startingDeal() {
+    let dealerHand = [randomDeal(deck), randomDeal(deck)];
+    let playerHand = [randomDeal(deck), randomDeal(deck)];
+    console.log(playerHand, dealerHand);
+}
+
+startGame.addEventListener('click', startingDeal);
+hitMe.addEventListener('click', randomDeal);
