e5c34c94e30d4d515d24230a90e50635f53ed3bd
maxmcgrath1 <tuc06096@temple.edu>
2022-01-04 15:18:49 -0500

    create deck

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,28 @@ let startGame = document.querySelector('.startGame');
 let welcomeMessage = document.querySelector('.welcomeMessage');
 let clickMessage = document.querySelector('.clickMessage')
 let hitMe = document.querySelector('.hitMe')
+
 startGame.addEventListener('click', function () {
     welcomeMessage.innerHTML = '';
     clickMessage.innerHTML = '';
 })
+
+let values = ['2', '3', '4', '5', '6', '7', '8','9', '10', 'J', 'Q', 'K', 'A'];
+let suits = ['clubs', 'diamonds', 'spades', 'hearts']
+
+let deck = [];
+
+function createDeck() {
+    for (let i=0; i < values.length; i++) {
+        for (let j= 0; j < suits.length; j++) {
+            // I want to create an object for each iteration that has key value pairs of suit and value
+            let cards = {
+                suit: suits[j], value: values[i]
+            };
+            deck.push(cards);
+        }
+    }
+}
+
+createDeck();
+console.log(deck);
