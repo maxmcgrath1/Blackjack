948519cc425d5fefc165b63f54c5c523f960c89c
maxmcgrath1 <tuc06096@temple.edu>
2022-01-04 17:50:13 -0500

    Add numerical values to cards array

--- scripts/app.js
+++ scripts/app.js
@@ -14,7 +14,7 @@ startGame.addEventListener('click', function () {
 
 // Here I am making arrays of both card values and the suits each value can be attached to
 
-let values = [{'2': 2}, '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+let values = [{'2': 2}, {'3': 3}, {'4': 4}, {'5': 5}, {'6': 6}, {'7': 7}, {'8': 8}, {'9': 9}, {'10': 10}, {'Jack': 10}, {'Queen': 10}, {'King': 10}, {'Ace': 1 || 11}];
 let suits = ['clubs', 'diamonds', 'spades', 'hearts']
 
 // Here I am creating a deck of 52 objects made of key value pairs from values and suits
