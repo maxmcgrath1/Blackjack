fb8e1c7d1df8c106fc453386535e8147b10e2e44
maxmcgrath1 <tuc06096@temple.edu>
2022-01-05 16:19:52 -0500

    Update hit card function to show card suits and values

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,25 @@
 // Here I am assigning variables to elements from the HTML
 
-let startGame = document.querySelector('.startGame');
 let welcomeMessage = document.querySelector('.welcomeMessage');
 let clickMessage = document.querySelector('.clickMessage');
+
+let startGame = document.querySelector('.startGame');
 let hitMe = document.querySelector('.hitMe');
+let stay = document.querySelector('.stay');
+
+let cardOne = document.querySelector('.cardOne');
+let cardTwo = document.querySelector('.cardTwo');
+
+let dealerFaceUp = document.querySelector('.dealerFaceUp');
+let dealerFaceDown = document.querySelector('.dealerFaceDown');
+
+let hitCardOne = document.querySelector('.hitCardOne');
+let hitCardTwo = document.querySelector('.hitCardTwo');
+let hitCardThree = document.querySelector('.hitCardThree');
+
+let stayCardOne = document.querySelector('.stayCardOne');
+let stayCardTwo = document.querySelector('.stayCardTwo');
+let stayCardThree = document.querySelector('.stayCardThree');
 
 // Here I am making a button that clears the welcome messages and starts the game
 
@@ -45,16 +61,26 @@ function randomDeal() {
 
 randomDeal();
 
+// I want to add one card to the player hand, using my random deal function 
+
 function hitCard () {
     let playerHitCard = randomDeal();
     playerHand.push(playerHitCard);
     console.log(playerHand)
+    if (hitCardOne.innerHTML === '') {
+        hitCardOne.innerHTML = playerHitCard.value + playerHitCard.suit;
+    } else
+        if (hitCardTwo.innerHTML === '') {
+            hitCardTwo.innerHTML = playerHitCard.value + playerHitCard.suit;
+        } else
+        if (hitCardThree.innerHTML === '') {
+            hitCardThree.innerHTML = playerHitCard.value + playerHitCard.suit;
+        }           
 }
 
 // Here I am trying to use my randomDeal function to initally deal 2 cards to the dealer and 2 cards to the player.
 
-// console.log(playerHand);
-// console.log(dealerHand);
+
 let playerHand = [];
 let dealerHand = [];
 
@@ -63,13 +89,24 @@ function startingDeal() {
     let playerCardTwo = randomDeal();
     let dealerCardOne = randomDeal();
     let dealerCardTwo = randomDeal();
-    playerHand.push(playerCardOne)
-    playerHand.push(playerCardTwo)
-    dealerHand.push(dealerCardOne)
-    dealerHand.push(dealerCardTwo)
-    console.log(playerHand)
-    console.log(dealerHand)
+    playerHand.push(playerCardOne);
+    playerHand.push(playerCardTwo);
+    dealerHand.push(dealerCardOne);
+    dealerHand.push(dealerCardTwo);
+    console.log(playerHand);
+    cardOne.innerHTML = playerCardOne.value + playerCardOne.suit;
+    cardTwo.innerHTML = playerCardTwo.value + playerCardTwo.suit;
+    console.log(cardOne);
+    console.log(playerCardOne.value);
+    console.log(cardTwo);
+    console.log(playerCardTwo.value);
+    console.log(dealerHand);
+    dealerFaceUp.innerHTML = dealerCardOne.value + dealerCardOne.suit;
+    dealerFaceDown.innerHTML = '?'
 }
 
 startGame.addEventListener('click', startingDeal);
 hitMe.addEventListener('click', hitCard);
+stay.addEventListener('click', function () {
+    dealerFaceDown.innerHTML = dealerCardTwo.value + dealerCardTwo.suit;
+});
